4aa21d02937211160ac38ec554033e0d22aa82da
Arif ansari <aa2736166@gmail.com>
2026-02-11 23:44:52 +0530

    rock,paper,scissor project  method 2

--- 100_projects_bootcamp.py
+++ 100_projects_bootcamp.py
@@ -230,3 +230,57 @@
 #     print("You lose!")
 # else:
 #     print("Invalid input")
+
+#METHOD 2 TO MAKE ROCK, PAPER SCISSORS GAME
+rock = ("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""")
+
+
+paper = ("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+
+scissors = ("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+import random
+
+game_images = [rock, paper, scissors]
+print("Welcome to rock, paper, scissors game!")
+user_input = int(input("choose 0 for rock, 1 for paper or 2 for scissors: "))
+
+print("Your choice:")
+print(game_images[user_input])
+
+computer = random.randint(0,2)
+print("computer choice:")
+print(game_images[computer])
+
+if user_input >= 3 or user_input < 0:
+    print("You typed an input. You lose!")
+elif user_input == computer:
+    print("Its a Draw")
+elif user_input == 0 and computer == 2:
+    print("You win")
+elif user_input == 2 and computer == 0:
+    print("You lose!")        
+elif user_input > computer:
+    print("You win!")
+elif computer > user_input:
+    print("You lose!")
